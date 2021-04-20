d83691118b7c50efaec929af16d35fec58254df2
truongminh2000 <76809227+truongminh2000@users.noreply.github.com>
2021-04-20 08:34:12 +0700

    update sql

--- Assets/Database/UniRoom.sql
+++ Assets/Database/UniRoom.sql
@@ -1,5 +1,8 @@
 Create Database UniRoom
+go
+
 Use UniRoom
+go
 
 Create table Account
 (
@@ -9,6 +12,7 @@ Create table Account
 	Phone varchar(12) not null,
 	Acc_Role char(1) not null
 )
+go
 
 Create table DiaChi
 (
@@ -17,10 +21,13 @@ Create table DiaChi
 	Quan nvarchar(50) not null, 
 	Truong nvarchar(50) not null
 )
+go
+
 
 Create table BaiDang
 (
 	Id_BaiDang varchar(10) primary key,
+	Username varchar(50),
 	Title nvarchar(255) not null,
 	NoiDung nvarchar(255) not null,
 	NgayDang date not null,
@@ -29,15 +36,30 @@ Create table BaiDang
 	HinhAnh varchar(255) not null,
 	TrangThai varchar(255) not null,
 	Id_DiaChi varchar(10) not null,
-	DiaChiCuThe varchar(255) not null,
+	DiaChiCuThe varchar(255) not null
 )
+go
+
+ALTER TABLE BaiDang
+ADD CONSTRAINT fk_id_DiaChi
+ FOREIGN KEY (Id_DiaChi)
+ REFERENCES DiaChi (Id_DiaChi);
+
+go
+
+ALTER TABLE BaiDang
+ADD CONSTRAINT fk_username
+ FOREIGN KEY (Username)
+ REFERENCES Account (Username);
 
 Create table FeedBack
 (
 	Id_FeedBack varchar(10) primary key,
-	Title nvarchar(255) not null,
-	NoiDung nvarchar(255) not null
+	full_Name nvarchar(100),
+	NoiDung nvarchar(255) not null,
+	email varchar(50)
 )
+go
 
 Create table Guide
 (
@@ -46,3 +68,4 @@ Create table Guide
 	NoiDung nvarchar(255) not null,
 	HinhAnh varchar(255) not null
 )
+go
