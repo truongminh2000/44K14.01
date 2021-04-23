8feda4efe0bd21718310ce9e1c4c894f6d6842bd
LuongNgocHoang <luongngochoang0@gmail.com>
2021-04-23 09:06:06 +0700

    Update UniRoom.sql

--- Assets/Database/UniRoom.sql
+++ Assets/Database/UniRoom.sql
@@ -156,3 +156,203 @@ Create table guides
 	picture varchar(255) not null
 )
 go
+
+
+
+
+
+
+
+
+-----------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 23, 2021 lúc 03:58 AM
+-- Phiên bản máy phục vụ: 10.4.17-MariaDB
+-- Phiên bản PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `uniroom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `districts`
+--
+
+CREATE TABLE `districts` (
+  `did` int(11) NOT NULL,
+  `dname` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `fid` int(11) NOT NULL,
+  `fullname` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `subject` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `guides`
+--
+
+CREATE TABLE `guides` (
+  `gid` int(11) NOT NULL,
+  `gname` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `posts`
+--
+
+CREATE TABLE `posts` (
+  `pid` int(11) NOT NULL,
+  `pname` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_520_ci NOT NULL,
+  `date_create` timestamp NOT NULL DEFAULT current_timestamp(),
+  `area` int(11) NOT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `status` int(1) NOT NULL,
+  `wid` int(11) NOT NULL,
+  `add_detail` text COLLATE utf8_unicode_520_ci NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `fullname` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `role_id` int(11) NOT NULL DEFAULT 2,
+  `enabled` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wards`
+--
+
+CREATE TABLE `wards` (
+  `wid` int(1) NOT NULL,
+  `wname` varchar(255) COLLATE utf8_unicode_520_ci NOT NULL,
+  `did` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `districts`
+--
+ALTER TABLE `districts`
+  ADD PRIMARY KEY (`did`);
+
+--
+-- Chỉ mục cho bảng `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`fid`);
+
+--
+-- Chỉ mục cho bảng `guides`
+--
+ALTER TABLE `guides`
+  ADD PRIMARY KEY (`gid`);
+
+--
+-- Chỉ mục cho bảng `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `wards`
+--
+ALTER TABLE `wards`
+  ADD PRIMARY KEY (`wid`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `districts`
+--
+ALTER TABLE `districts`
+  MODIFY `did` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `guides`
+--
+ALTER TABLE `guides`
+  MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `wards`
+--
+ALTER TABLE `wards`
+  MODIFY `wid` int(1) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
